911974faebc8f7ddf7856b5ecfd577d5fdccf990
KoalaYuFeng <yufeng@nus.edu.sg>
2023-03-04 21:20:42 +0800

    add 512 bitwidth support

--- acc_impl/src/tc_kernel.cpp
+++ acc_impl/src/tc_kernel.cpp
@@ -5,7 +5,9 @@
 
 
 typedef struct v_datatype {int data[16];} int512;
-#define BUF_DEPTH   65536
+#define BUF_DEPTH   4096
+#define T           16
+#define BURST_LEN   8
 // use 512 bit width to full utilize bandwidth
 
 template <typename DT>
@@ -19,30 +21,21 @@ void burstReadStrm(int length, DT* inArr, hls::stream<DT>& outStrm) {
 
 
 // need to add test txt file for debug.
-template <typename DT>
-void adjListCpy(DT* dst, int512* start_location, int offset, int len){
-    int T = 16; // 512/32=16
+void adjListCpy(int512* dst, int512* start_location, int offset, int len){
     int offset_begin = offset / T;
     int offset_end = (offset + len + T - 1) / T;
+    int loop_num = (offset_end - offset_begin + BURST_LEN - 1) / BURST_LEN;
 
-    for (int i = offset_begin; i < offset_end; i++) {
-#pragma HLS loop_tripcount min = 1000 max = 1000
-#pragma HLS pipeline ii = 1
-        for (unsigned int j = 0; j < T; j++) {
+    for (int i = 0; i < loop_num; i++) {
+        for (int j = 0; j < BURST_LEN; j++) {
+#pragma HLS pipeline II=1
+            if ((i*BURST_LEN + j) < (offset_end - offset_begin)) {
+                for (int k = 0; k < T; k++) {
 #pragma HLS unroll
-            if (((i*T + j) < (offset + len)) && ((i*T + j) >= offset)) {
-                dst[i*T + j - offset] = start_location[i].data[j];
+                    dst[i*BURST_LEN + j].data[k] = start_location[i*BURST_LEN + j + offset_begin].data[k];
+                }
             }
         }
-    }
-}
-
-template <typename DT>
-void burstCpyArray(DT* inPort, DT* dstArr, int offset, int length) {
-    for (int i = 0; i < length; i++) {
-#pragma HLS loop_tripcount min = 1000 max = 1000
-// can not add ii = 1
-        dstArr[i] = inPort[i + offset];
     }
 }
 
@@ -90,45 +83,57 @@ int msb(unsigned int v) {
 	return pos[(v* 0x077CB531) >> 27];
 }
 
-
-template <typename DT>
-int lbitBinarySearch(DT arr[], int low, int high, DT key){
+int lbitBinarySearch(int512 arr[], int low, int high, int offset, int key){
     // int k = highestPowerof2(high);
+    int init = offset & 0xf;
     int lbit = msb(high);
+
     int k = high ? 1 << lbit : 0;
-    // int k = 0;
-    // if (high > 0) {
-    //     int pos = log2(high);
-    //     k = 1 << pos;
-    // }
-    int temp_value = arr[k];
-    int r;
+    int idx_k = k + init;
+    int block_k = idx_k >> 4;
+    int bias_k = idx_k & 0xf;
+    int temp_value = arr[block_k].data[bias_k];
+
+    int r, idx_r, block_r, bias_r;
     int i = (temp_value <= key) ? k : 0;
+    int idx_i, block_i, bias_i;
     while ((k >>= 1)){
 #pragma HLS pipeline ii=1
         r = i + k;
-        temp_value = arr[r];
+        idx_r = r + init;
+        block_r = idx_r >> 4;
+        bias_r = idx_r & 0xf;
+        temp_value = arr[block_r].data[bias_r];
+
         if ((r <= high) && (key > temp_value)) {
             i = r;
         } else if ((r <= high) && (key == temp_value)) {
             return r;
         }
     }
-    if (arr[i] == key) {
+
+    idx_i = i + init;
+    block_i = idx_i >> 4;
+    bias_i = idx_i & 0xf;
+    temp_value = arr[block_i].data[bias_i];
+    if (temp_value == key) {
         return i;
     } else {
         return -1;
     }
 }
 
-template <typename DT>
-void setInterBinarySearch(DT *list_a, DT *list_b, int len_a, int len_b, int* tc_cnt){
+void setInterBinarySearch(int512 *list_a, int512 *list_b, int len_a, int len_b, int offset_a, int offset_b, int* tc_cnt){
 /** Need to make sure the list_a is the shorter one **/
     unsigned int count = 0;
     int temp_idx_0 = 0;
     // list_a has to be the shorter one, search with element from shorter array
     setint: for (int i = 0; i < len_a; i++){
-        temp_idx_0 = lbitBinarySearch<DT>(list_b, 0, len_b-1, list_a[i]);
+        int ini_idx_a = offset_a & 0xf;
+        int idx = ini_idx_a + i;
+        int block_a = idx >> 4;
+        int bias_a = idx & 0xf;
+        temp_idx_0 = lbitBinarySearch(list_b, 0, len_b-1, offset_b, list_a[block_a].data[bias_a]);
         // temp_idx_0 = binarySearch<DT>(list_b, 0, len_b-1, list_a[i]);
         if (temp_idx_0 != -1) {
             count++;
@@ -153,17 +158,30 @@ void setInterBinarySearchMT(DT *list_a, DT *list_b, int len_a, int len_b, int* t
     tc_cnt[0] = count;
 }
 
-void setIntersection(int *list_a, int *list_b, int len_a, int len_b, int offset_1, int offset_2,  int* tc_cnt)
+void setIntersection(int512 *list_a, int512 *list_b, int len_a, int len_b, int offset_a, int offset_b, int* tc_cnt)
 {
+    #pragma HLS inline off
     int count = 0;
     int idx_a = 0, idx_b = 0;
+    int initial_512_a = offset_a & 0xf;
+    int initial_512_b = offset_b & 0xf;
     while ((idx_a < len_a) && (idx_b < len_b))
     {
-#pragma HLS pipeline ii = 1
-#pragma HLS loop_tripcount min = 20 max = 20
-        if(list_a[idx_a + offset_1] < list_b[idx_b + offset_2])
+#pragma HLS pipeline ii=1
+
+        int a_512_t = initial_512_a + idx_a;
+        int a_512_block = a_512_t >> 4;
+        int a_512_bias = a_512_t & 0xf;
+        int value_a = list_a[a_512_block].data[a_512_bias];
+
+        int b_512_t = initial_512_b + idx_b;
+        int b_512_block = b_512_t >> 4;
+        int b_512_bias = b_512_t & 0xf;
+        int value_b = list_b[b_512_block].data[b_512_bias];
+
+        if(value_a < value_b)
             idx_a++;
-        else if (list_a[idx_a + offset_1] > list_b[idx_b + offset_2])
+        else if (value_a > value_b)
             idx_b++;
         else {
             count++;
@@ -173,46 +191,6 @@ void setIntersection(int *list_a, int *list_b, int len_a, int len_b, int offset_
     }
     tc_cnt[0] = count;
 }
-
-void setIntersection(int *list_a, int *list_b, int len_a, int len_b, int* tc_cnt)
-{
-    int count = 0;
-    int idx_a = 0, idx_b = 0;
-    while ((idx_a < len_a) && (idx_b < len_b))
-    {
-#pragma HLS pipeline ii = 1
-#pragma HLS loop_tripcount min = 20 max = 20
-        if(list_a[idx_a] < list_b[idx_b])
-            idx_a++;
-        else if (list_a[idx_a] > list_b[idx_b])
-            idx_b++;
-        else {
-            count++;
-            idx_a++;
-            idx_b++;
-        }
-    }
-    tc_cnt[0] = count;
-}
-
-void setIntersectionForloop(int *list_a, int *list_b, int len_a, int len_b, int offset_1, int offset_2,  int* tc_cnt)
-{
-    int count = 0;
-    for (int idx_a = 0, idx_b = 0; idx_a < len_a && idx_b < len_b;)
-    {
-        if(list_a[idx_a + offset_1] < list_b[idx_b + offset_2])
-            idx_a++;
-        else if (list_a[idx_a + offset_1] > list_b[idx_b + offset_2])
-            idx_b++;
-        else {
-            count++;
-            idx_a++;
-            idx_b++;
-        }
-    }
-    tc_cnt[0] = count;
-}
-
 
 extern "C" {
 void TriangleCount(int* edge_list, int* offset_list_1, int* offset_list_2, int512* column_list_1, int512* column_list_2, int edge_num, int* tc_number ) {
@@ -240,8 +218,8 @@ void TriangleCount(int* edge_list, int* offset_list_1, int* offset_list_2, int51
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
 // Initial local memory space of list_a and list_b
-    int list_a[BUF_DEPTH];
-    int list_b[BUF_DEPTH];
+    int512 list_a[BUF_DEPTH];
+    int512 list_b[BUF_DEPTH];
 // #pragma HLS RESOURCE variable=list_a core=XPM_MEMORY uram
 // #pragma HLS RESOURCE variable=list_b core=XPM_MEMORY uram
     int edge_list_buf[32];
@@ -285,12 +263,12 @@ void TriangleCount(int* edge_list, int* offset_list_1, int* offset_list_2, int51
             // int node_b = b_index_buf[k*2];
             int vertex_a_idx = a_index_buf[k*2];
             int vertex_b_idx = b_index_buf[k*2];
-            std::cout<< "read nodes: "<< node_a <<", "<<node_b << std::endl;
+            // std::cout<< "read nodes: "<< node_a <<", "<<node_b << std::endl;
             // int len_a = offset_list_1[node_a + 1] - vertex_a_idx;
             // int len_b = offset_list_2[node_b + 1] - vertex_b_idx;
             int len_a = len_a_buf[k];
             int len_b = len_b_buf[k];
-            std::cout<< "lens of lists: "<< len_a <<", "<< len_b << std::endl;
+            // std::cout<< "lens of lists: "<< len_a <<", "<< len_b << std::endl;
             
             int short_list = 0;
             int long_list = 0;
@@ -299,33 +277,26 @@ void TriangleCount(int* edge_list, int* offset_list_1, int* offset_list_2, int51
                 // burstCpyArray<int>(column_list_1, list_a, len_a, vertex_a_idx);
                 // burstCpyArray<int>(column_list_2, list_b, len_b, vertex_b_idx);
                 if (previous_node_a != node_a){
-                    adjListCpy<int>(list_a, column_list_1, vertex_a_idx, len_a);   
+                    adjListCpy(list_a, column_list_1, vertex_a_idx, len_a);   
                     previous_node_a = node_a; 
                 }
-                adjListCpy<int>(list_b, column_list_2, vertex_b_idx, len_b);
+                adjListCpy(list_b, column_list_2, vertex_b_idx, len_b);
 
                 // Process setintersection on lists with the lens are not zeros
                 int temp_count[1] = {0};
                 // setIntersection(column_list_1, column_list_2, len_a, len_b, vertex_a_idx, vertex_b_idx, temp_count);
                 // setIntersection(list_a, list_b, len_a, len_b, temp_count);
                 /**/
-                if (len_a < len_b){
-                    short_list = len_a;
-                    long_list = len_b;
+
+                if(len_b / len_a >= 32){
+                    setInterBinarySearch(list_a, list_b, len_a, len_b, vertex_a_idx, vertex_b_idx, temp_count);
+                } else if (len_a / len_b >= 32) {
+                    setInterBinarySearch(list_b, list_a, len_b, len_a, vertex_b_idx, vertex_a_idx, temp_count);
                 } else {
-                    short_list = len_b;
-                    long_list = len_a;
+                    setIntersection(list_a, list_b, len_a, len_b, vertex_a_idx, vertex_b_idx, temp_count);
                 }
 
-                if(long_list / short_list >= 32){
-                    if (len_a <= len_b){
-                        setInterBinarySearch<int>(list_a, list_b, len_a, len_b, temp_count);
-                    } else {
-                        setInterBinarySearch<int>(list_b, list_a, len_b, len_a, temp_count);
-                    }
-                } else {
-                    setIntersection(list_a, list_b, len_a, len_b, temp_count);
-                }
+                // setIntersection(list_a, list_b, len_a, len_b, vertex_a_idx, vertex_b_idx, temp_count);
                 /**/
                 triangle_count += temp_count[0];
             }
