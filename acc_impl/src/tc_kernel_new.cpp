3afda49fd80291483e569c3a6b6e0ac527e33367
KoalaYuFeng <yufeng@nus.edu.sg>
2023-03-24 13:21:02 +0800

    add multi-insec module, but sequential exe :-(

--- acc_impl/src/tc_kernel_new.cpp
+++ acc_impl/src/tc_kernel_new.cpp
@@ -3,46 +3,48 @@
 #include <hls_math.h>
 #include <iostream>
 
-
+// use 512 bit width to full utilize bandwidth
 typedef struct v_datatype {int data[16];} int512;
 #define BUF_DEPTH   512
 #define T           16
 #define T_offset    4
 #define BURST_LEN   8
-#define MAX_colase  64
+#define MAX_colase  512
+#define InSecNum    4
 const int c_size = BUF_DEPTH;
-// use 512 bit width to full utilize bandwidth
 
-
-// need to add test txt file for debug.
-void adjListCpy(int* dst, int512* start_location, int offset, int len){
-    
-    int offset_begin = offset / T;
-    int offset_end = (offset + len + T - 1) / T;
-    int offset_idx = offset & 0xf;
-    for (int i = offset_begin; i < offset_end; i++) {
+void adjListCpy(int dst[BUF_DEPTH][T], int512* src, int begin, int end) {
+#pragma HLS inline
+    for (int i = begin; i < end; i++) {
 #pragma HLS pipeline
-        int512 temp = start_location[i];
+        int512 temp_a = src[i];
         for (int j = 0; j < T; j++) {
 #pragma HLS unroll
-            int t = (i - offset_begin) * T + j - offset_idx;
-            if (t >= 0)
-                dst[t] = temp.data[j];
+            dst[i - begin][j] = temp_a.data[j];
         }
     }
 }
 
-void setIntersection(int* list_a, int* list_b, int len_a, int len_b, int* tc_cnt)
+// since list b contains multiple adj lists, so need an extra offset.
+void setIntersection(int list_a[BUF_DEPTH][T], int list_b[BUF_DEPTH][T], int list_a_offset, int list_a_len, int list_b_offset, int list_b_offset_t, int list_b_len_t, int* tc_count)
 {
-#pragma HLS inline off
+#pragma HLS inline
+    int o_idx_a = list_a_offset & 0xf;
+    int o_idx_b = (list_b_offset & 0xf) + (list_b_offset_t - list_b_offset);
+    int idx_a = o_idx_a;
+    int idx_b = o_idx_b;
     int count = 0;
-    int idx_a = 0, idx_b = 0;
-    while ((idx_a < len_a) && (idx_b < len_b))
+loop_set_inetrsection: while ((idx_a < list_a_len + o_idx_a) && (idx_b < list_b_len_t + o_idx_b))
     {
 #pragma HLS pipeline ii=1
+        int a_dim_1 = idx_a >> T_offset;
+        int a_dim_2 = idx_a & 0xf;
 
-        int value_a = list_a[idx_a];
-        int value_b = list_b[idx_b];
+        int b_dim_1 = idx_b >> T_offset;
+        int b_dim_2 = idx_b & 0xf;
+
+        int value_a = list_a[a_dim_1][a_dim_2];
+        int value_b = list_b[b_dim_1][b_dim_2];
 
         if(value_a < value_b)
             idx_a++;
@@ -54,7 +56,7 @@ void setIntersection(int* list_a, int* list_b, int len_a, int len_b, int* tc_cnt
             idx_b++;
         }
     }
-    tc_cnt[0] = count;
+    tc_count[0] = count;
 }
 
 template <typename DT>
@@ -127,6 +129,8 @@ void colase_request (int edge_num, hls::stream<DT>& edge_strm, \
             a_colase_strm << true;
             b_colase_strm << true;
             end_stream << false;
+
+            b_colase_len_strm << b_valid_idx; // write a coalesce number
             for (int j = 0; j <= b_valid_idx - 1; j++ ) {
                 b_colase_idx_strm << b_idx_buffer;
                 b_colase_len_strm << b_len_buffer[j];
@@ -157,6 +161,8 @@ void colase_request (int edge_num, hls::stream<DT>& edge_strm, \
     a_colase_strm << true;
     b_colase_strm << true;
     end_stream << false;
+
+    b_colase_len_strm << b_valid_idx; // write a coalesce number
     for (int j = 0; j <= b_valid_idx - 1; j++ ) {
         b_colase_idx_strm << b_idx_buffer;
         b_colase_len_strm << b_len_buffer[j];
@@ -187,6 +193,7 @@ void procIntersec (int512* column_list_1, int512* column_list_2, int length, int
     int list_a_offset;
     int list_b_len;
     int list_b_offset;
+    int list_a_previous = -1;
 
     bool end_flag = end_stream.read();
     while (!end_flag) {
@@ -194,7 +201,6 @@ void procIntersec (int512* column_list_1, int512* column_list_2, int length, int
         bool b_colase = b_colase_strm.read();
 
         if (a_colase && b_colase) {
-#pragma HLS dataflow
             // adj list cpy.
             list_a_len = a_colase_len_strm.read();
             list_a_offset = a_colase_idx_strm.read();
@@ -206,58 +212,37 @@ void procIntersec (int512* column_list_1, int512* column_list_2, int length, int
             int o_begin_b = list_b_offset >> T_offset;
             int o_end_b = (list_b_offset + list_b_len + T - 1) >> T_offset;
 
-            loop_a_adj_cpy: for (int i = o_begin_a; i < o_end_a; i++) {
-#pragma HLS pipeline
-                int512 temp_a = column_list_1[i];
-                for (int j = 0; j < T; j++) {
+            if (list_a_previous != list_a_offset) { // a simple cache for list_a;
+                adjListCpy(list_a, column_list_1, o_begin_a, o_end_a);
+            }
+            list_a_previous = list_a_offset;
+
+            adjListCpy(list_b, column_list_2, o_begin_b, o_end_b);
+
+            int list_b_num = b_colase_len_strm.read(); // read a coalesce number
+            int b_len[MAX_colase];
+            int b_idx[MAX_colase];
+
+            for (int i = 0; i < list_b_num; i++) {
+                b_len[i] = b_colase_len_strm.read();
+                b_idx[i] = b_colase_idx_strm.read();
+                a_colase = a_colase_strm.read();
+                b_colase = b_colase_strm.read();
+                end_flag = end_stream.read();
+            }
+
+            int loop_num = (list_b_num + InSecNum - 1) / InSecNum;
+
+            for (int i = 0; i < loop_num; i++) {
+                for (int j = 0; j < InSecNum; j++) {
 #pragma HLS unroll
-                    list_a[i - o_begin_a][j] = temp_a.data[j];
+                    if ((i*InSecNum + j) < list_b_num) {
+                        int count[1] = {0};
+                        setIntersection(list_a, list_b, list_a_offset, list_a_len, list_b_offset, b_idx[i*InSecNum + j], b_len[i*InSecNum + j], count);
+                        triangle_count += count[0];
+                    }
                 }
             }
-
-            loop_b_adj_cpy: for (int i = o_begin_b; i < o_end_b; i++) {
-#pragma HLS pipeline
-                int512 temp_b = column_list_2[i];
-                for (int j = 0; j < T; j++) {
-#pragma HLS unroll
-                    list_b[i - o_begin_b][j] = temp_b.data[j];
-                }
-            }
-        }
-
-        if ((!a_colase) && (!b_colase)) {
-
-            int list_b_len_t = b_colase_len_strm.read();
-            int list_b_offset_t = b_colase_idx_strm.read();
-
-            int count = 0;
-            int o_idx_a = list_a_offset & 0xf;
-            int o_idx_b = (list_b_offset & 0xf) + (list_b_offset_t - list_b_offset);
-            int idx_a = o_idx_a;
-            int idx_b = o_idx_b;
-            loop_set_inetrsection: while ((idx_a < list_a_len + o_idx_a) && (idx_b < list_b_len_t + o_idx_b))
-            {
-    #pragma HLS pipeline ii=1
-                int a_dim_1 = idx_a >> T_offset;
-                int a_dim_2 = idx_a & 0xf;
-
-                int b_dim_1 = idx_b >> T_offset;
-                int b_dim_2 = idx_b & 0xf;
-
-                int value_a = list_a[a_dim_1][a_dim_2];
-                int value_b = list_b[b_dim_1][b_dim_2];
-
-                if(value_a < value_b)
-                    idx_a++;
-                else if (value_a > value_b)
-                    idx_b++;
-                else {
-                    count++;
-                    idx_a++;
-                    idx_b++;
-                }
-            }
-            triangle_count += count;
         }
 
         end_flag = end_stream.read();
